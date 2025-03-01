fb8698a88d473f9c07e028b0d59e5931f2abb93f
knewcid1234 <knewcid1234@gmail.com>
2025-03-01 10:05:49 -0500

    portfolio skeleton

---
+++ portfolio/src/components/Homepage.js
@@ -0,0 +1,8 @@
+const Homepage = () => {
+    <aside className='portfolio'>
+        <img src='..\public\logo512.png' ></img>
+        <h1>Hello World!!</h1>
+    </aside>
+}
+
+export default Homepage;
